cbd372590bbb5c3d7d067606bf3801429f5c9500
Gorungas <50162737+Gorungas@users.noreply.github.com>
2021-04-20 20:56:38 -0700

    Added Camera Tracking and Basic player movement

---
+++ REAL REAL PROJ/Assets/PlayerMovement.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float speed = 10.0f;
+    public float rotationSpeed = 100.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float trans = Input.GetAxis("Vertical") * speed;
+        float rotation = Input.GetAxis("Horizontal") * rotationSpeed;
+
+        trans *= Time.deltaTime;
+        rotation *= Time.deltaTime;
+
+        transform.Translate(0, 0, trans);
+
+        transform.Rotate(0, rotation, 0);
+
+    }
+
+    
+}
