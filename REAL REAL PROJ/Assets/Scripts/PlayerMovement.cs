c50429241110c2caa919acd63811b3c3ae30457b
Gorungas <50162737+Gorungas@users.noreply.github.com>
2021-04-23 22:25:57 -0700

    fixed gravity switching mechanic

--- REAL REAL PROJ/Assets/Scripts/PlayerMovement.cs
+++ REAL REAL PROJ/Assets/Scripts/PlayerMovement.cs
@@ -6,7 +6,6 @@ public class PlayerMovement : MonoBehaviour
 {
     public float speed = 100.0f;
     public float rotationSpeed = 100.0f;
-    public float gravityPower = 1.0f;
     public Vector3 gravity;
     public Transform tpOut;
 
@@ -19,13 +18,12 @@ public class PlayerMovement : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        gravity = new Vector3(0f, -2.0f, 0f);
+        gravity = new Vector3(0f, -3.0f, 0f);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
         float trans = Input.GetAxis("Vertical") * speed;
         float rotation = Input.GetAxis("Horizontal") * rotationSpeed;
         if (!move2d)
@@ -37,7 +35,6 @@ public class PlayerMovement : MonoBehaviour
             playerRigid.AddForce(rotation, 0, 0);
         }
 
-        //Physics.gravity = new Vector3(0, gravity, 0);
 
         if (move2d)
         {
@@ -45,15 +42,15 @@ public class PlayerMovement : MonoBehaviour
 
             trans *= Time.deltaTime;
             playerRigid.AddForce(0, 0, trans);
-            
+
             playerRigid.AddForce(gravity);
+            playerRigid.useGravity = false;
 
-            
 
-            if (Input.GetKeyDown("Jump"))
+            if (Input.GetKeyDown(KeyCode.Space))
             {
                 gravity *= -1;
-               
+
             }
 
         }
@@ -64,6 +61,6 @@ public class PlayerMovement : MonoBehaviour
         isOnGround = true;
     }
 
-    
+
 
 }
